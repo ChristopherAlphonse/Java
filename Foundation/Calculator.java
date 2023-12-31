86ab60511236d18a576ac49856d8c7184aa0d888
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-12-31 16:04:06 -0500

    refactor code to use, using sonarlint for clean code, using lambda, logger, and switch case instead of nested if else

--- Foundation/Calculator.java
+++ Foundation/Calculator.java
@@ -1,49 +1,63 @@
 package Foundation;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Calculator {
+
+    private static final Logger logger = Logger.getLogger(Calculator.class.getName());
+
     public static void main(String[] args) {
-
         Scanner userIn = new Scanner(System.in);
-        System.out.print("Enter the operator: ");
-
-        int answer = 0;
 
         while (true) {
-            char op = userIn.next().trim().charAt(0);
+            char op = getOperator(userIn);
 
-            if (op == '+' || op == '-' || op == '/' || op == '%' || op == '*') {
-                System.out.print("Enter two numbers: ");
-                int a = userIn.nextInt();
-                int b = userIn.nextInt();
-
-                if (op == '+') {
-                    answer = a + b;
-                } else if (op == '-') {
-                    answer = a - b;
-                } else if (op == '/') {
-                    if (b != 0) {
-                        answer = a / b;
-                    } else {
-                        System.out.println("Cannot divide by zero.");
-                        continue;
-                    }
-                } else if (op == '%') {
-                    answer = a % b;
-                } else {
-                    answer = a * b;
-                }
-
-                System.out.println("Result: " + answer);
-            } else if (op == 'x' || op == 'X') {
+            if (op == 'x' || op == 'X') {
                 break;
-            } else {
-                System.out.println("Invalid Try again.");
             }
 
+            performOperation(op, userIn);
         }
-        userIn.close();
 
+        userIn.close();
+    }
+
+    private static char getOperator(Scanner userIn) {
+        logger.info("Enter the operator: ");
+        return userIn.next().trim().charAt(0);
+    }
+
+    private static void performOperation(char op, Scanner userIn) {
+        if (isValidOperator(op)) {
+            logger.info("Enter two numbers: ");
+            int a = userIn.nextInt();
+            int b = userIn.nextInt();
+
+            int answer = calculate(op, a, b);
+            logger.info("Result: " + answer);
+        } else {
+            logger.warning("Invalid operator. Try again.");
+        }
+    }
+
+    private static boolean isValidOperator(char op) {
+        return op == '+' || op == '-' || op == '/' || op == '%' || op == '*';
+    }
+
+    private static int calculate(char op, int a, int b) {
+        return switch (op) {
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '/' -> (b != 0) ? a / b : handleDivisionByZero();
+            case '%' -> a % b;
+            case '*' -> a * b;
+            default -> 0;
+        };
+    }
+
+    private static int handleDivisionByZero() {
+        logger.info("Cannot divide by zero.");
+        return 0;
     }
 }
