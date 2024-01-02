bb912a076cb1a912f9e3a4ebe9b5b6c5472511d3
ChristopherAlphonse <christopheralphonse96@gmail.com>
2024-01-01 20:50:33 -0500

    update

--- com/foundation/NestedSwitch.java
+++ com/foundation/NestedSwitch.java
@@ -1,4 +1,37 @@
 package com.foundation;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class NestedSwitch {
+
+    private static final Logger logger = Logger.getLogger(NestedSwitch.class.getName());
+
+    public static void main(String[] args) {
+        Scanner userIn = new Scanner(System.in);
+
+
+        System.out.print("Enter Identification Number: ");
+        int userID = userIn.nextInt();
+        userIn.nextLine();
+
+        switch (userID) {
+            case 1 -> logger.info("John Doe");
+            case 2 -> logger.info("Mary Jane");
+            case 3 -> {
+                logger.info("Enter Department: ");
+                String department = userIn.next().toUpperCase();
+                switch (department) {
+                    case "IT" -> logger.info("IT Department");
+                    case "HR" -> logger.info("HR Department");
+                    default -> logger.warning("Department not found");
+                }
+            }
+            default -> logger.warning("try again");
+        }
+
+        userIn.close();
+    }
+
+
 }
