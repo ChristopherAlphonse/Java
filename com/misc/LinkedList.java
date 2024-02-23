036f41d35e2b06eee3d701c1743d3883fa1f4e17
christopheralphonse <christopheralphonse96@gmail.com>
2024-02-23 18:29:01 -0500

    update

--- com/misc/LinkedList.java
+++ com/misc/LinkedList.java
@@ -1,12 +1,10 @@
 package com.misc;
 
-<<<<<<< HEAD
-public class LinkedList{
+
+public class LinkedList {
 	private Node head;
 	private Node tail;
 	private int length;
-
-
 
 
 	class Node {
@@ -14,8 +12,7 @@ public class LinkedList{
 		Node next;
 
 
-
-		Node(int data){
+		Node(int data) {
 			this.data = data;
 
 
@@ -23,103 +20,4 @@ public class LinkedList{
 	}
 
 
-
-
-
-
-
-
-
 }
-=======
-
-public class LinkedList {
-
-
-    private Node head;
-    private Node tail;
-    private int length;
-
-
-    class Node {
-
-        int value;
-        Node next;
-
-        Node(int value) {
-            this.value = value;
-
-        }
-
-    }
-
-    public LinkedList(int value) {
-        Node newNode = new Node(value);
-        head = newNode;
-        tail = newNode;
-        length = 1;
-    }
-
-
-    public Node getHead() {
-        return head;
-    }
-
-    public Node getTail() {
-        return tail;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public void printList() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.value);
-            temp = temp.next;
-        }
-    }
-
-    public void printAll() {
-        if (length == 0) {
-            System.out.println("Head: null");
-            System.out.println("Tail: null");
-        } else {
-            System.out.println("Head: " + head.value);
-            System.out.println("Tail: " + tail.value);
-        }
-        System.out.println("Length:" + length);
-        System.out.println("\nLinked List:");
-        if (length == 0) {
-            System.out.println("empty");
-        } else {
-            printList();
-        }
-    }
-
-
-    public static void main(String[] args) {
-
-        LinkedList myLinkedList = new LinkedList(4);
-
-        myLinkedList.printAll();
-
-
-        /*
-            EXPECTED OUTPUT:
-            ----------------
-            Head: 4
-            Tail: 4
-            Length: 1
-
-            Linked List:
-            4
-
-        */
-
-    }
-
-}
-
->>>>>>> 38c2785d19b2bf4f59e7da2c2c85c0b337cc7d6f
