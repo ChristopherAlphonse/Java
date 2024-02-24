37bf1532b6fc9cb022179ac136fae51d38111ed1
christopheralphonse <christopheralphonse96@gmail.com>
2024-02-23 19:40:35 -0500

    add append

--- com/misc/LinkedList.java
+++ com/misc/LinkedList.java
@@ -1,23 +1,66 @@
 package com.misc;
 
-
 public class LinkedList {
 	private Node head;
 	private Node tail;
 	private int length;
 
-
 	class Node {
 		int data;
 		Node next;
 
-
 		Node(int data) {
 			this.data = data;
-
-
+			this.next = null;
 		}
 	}
 
+	public LinkedList(int d) {
+		Node newNodeObject = new Node(d);
+		head = newNodeObject;
+		tail = newNodeObject;
+		length = 1;
+	}
 
+	public void printList() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
+	public void getHeadNode() {
+		System.out.println(head.data);
+	}
+
+	public void getTailNode() {
+		System.out.println(tail.data);
+	}
+
+	public void getLengthOfNode() {
+		System.out.println(length);
+	}
+
+
+	public void append(int data) {
+		Node newNodeData = new Node(data);
+
+		if (length == 0) {
+			head = newNodeData;
+			tail = newNodeData;
+		} else {
+			tail.next = newNodeData;
+			tail = newNodeData;
+		}
+		length++;
+	}
+}
+
+
+public static void main(String[] args) {
+	LinkedList myLinkedList = new LinkedList(5);
+	myLinkedList.printList();
+
+}
 }
