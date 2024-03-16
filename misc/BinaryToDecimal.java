9a094d6affe094f92861d0f0d6143d14358c0412
christopheralphonse <christopheralphonse96@gmail.com>
2024-03-16 11:04:47 -0400

    update

---
+++ misc/BinaryToDecimal.java
@@ -0,0 +1,16 @@
+package misc;
+
+import misc.LinkedList.Node;
+
+public class BinaryToDecimal {
+    public int binaryToDecimal(Node head) {
+        int num = 0;
+        Node current = head;
+        while (current != null) {
+            num = (num * 2) + current.data;
+            current = current.next;
+        }
+        return num;
+    }
+
+}
