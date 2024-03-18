da50d84b20da278b9e16757dcacc3c6496714b7b
christopheralphonse <christopheralphonse96@gmail.com>
2024-03-17 23:12:59 -0400

    :happy:

--- leetcode/String/ReverseString.java
+++ leetcode/String/ReverseString.java
@@ -1,15 +1,45 @@
-import java.util.Stack;
 
+//
+// Big0(n)
+// class Solution {
+// public void reverseString(char[] s) {
+// Stack<Character> result = new Stack<>();
+
+// for(int i = 0; i < s.length; i++ ) {
+// result.push(s[i]);
+// }
+
+// for(int i = 0; i < s.length; i++ ) {
+// s[i] = result.pop();
+// }
+// }
+// }
+
+// 0(1) space
+// two pointer, dive and conquer, string
 class Solution {
     public void reverseString(char[] s) {
-        Stack<Character> result = new Stack<>();
+        // start at the beginning
+        int left = 0;
+        // start at end
+        int right = s.length - 1;
 
-        for (int i = 0; i < s.length; i++) {
-            result.push(s[i]);
+        while (left <= right) {
+            // method to clarify my idea
+            swap(s, left, right);
+
+            // move up 1
+            left++;
+            // move down 1
+            right--;
         }
 
-        for (int i = 0; i < s.length; i++) {
-            s[i] = result.pop();
-        }
+    }
+
+    // custom swap method
+    private void swap(char[] s, int a, int b) {
+        char temp = s[a];
+        s[a] = s[b];
+        s[b] = temp;
     }
 }
