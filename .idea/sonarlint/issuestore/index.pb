5eaee2693f7f11b6e3e407a7f759491f93df403f
christopheralphonse <christopheralphonse96@gmail.com>
2024-03-22 20:48:05 -0400

    update

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+\
+,leetcode/Array/containsDups/containsDup.java,a\8\a86b12687b54cce8afcaeacc5db6447804d106d2
---
+++ CrackingTheCode/pairSequence.java
@@ -0,0 +1,23 @@
+package CrackingTheCode;
+
+public class pairSequence {
+
+    public static int pairSumSequence(int n) {
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += pairSum(i, i + 1);
+        }
+
+        return sum;
+    }
+
+    static int pairSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pairSumSequence(2));
+
+    }
+}
